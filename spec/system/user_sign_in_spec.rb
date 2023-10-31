1f017d356cd82f03e08255430be1890c679fe088
vando <vandorramos@gmail.com>
2023-10-31 14:03:16 -0300

    Install and setup gems - Rspec, Capybara and Devise. Create a system test for user authenticates, create user model, setup root route,create home controller and create view index.

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'User authenticates' do
+  it 'sucessfully' do
+    User.create!(email: 'host@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Sign in'
+    within('form') do
+      fill_in 'Email', with: 'host@email.com'
+      fill_in 'Password', with: '123456'
+      click_on 'Sign in'
+    end
+
+    expect(page).to have_content('Signed in successfully')
+    within('nav') do
+      expect(page).not_to have_link('Sign in')
+      expect(page).to have_button('Sign out')
+      expect(page).to have_content('host@email.com')
+    end
+  end
+end
