328672999d9932beb7315c37f07ffa9c49bef44e
vando <vandorramos@gmail.com>
2023-10-31 16:52:43 -0300

    Create sign out button

--- spec/system/authentication/user_sign_in_spec.rb
+++ spec/system/authentication/user_sign_in_spec.rb
@@ -19,4 +19,24 @@ describe 'User authenticates' do
       expect(page).to have_content('host@email.com')
     end
   end
+
+  it 'and signs out' do
+    User.create!(email: 'host@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Sign in'
+    within('form') do
+      fill_in 'Email', with: 'host@email.com'
+      fill_in 'Password', with: '123456'
+      click_on 'Sign in'
+    end
+    click_on 'Sign out'
+
+    expect(page).to have_content('Signed out successfully')
+    expect(page).to have_link('Sign in')
+    expect(page).not_to have_button('Sign out')
+    within('nav') do
+      expect(page).not_to have_content('host@email.com')
+    end
+  end
 end
